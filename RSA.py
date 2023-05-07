751a05b9522bda34389121cde73a64bc85517fdc
CR2100 <104399936+CR2100@users.noreply.github.com>
2023-05-07 10:12:52 -0500

    Final Version Update

--- RSA.py
+++ RSA.py
@@ -4,8 +4,8 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 
-# define the encryption function for each file type
 def encrypt_file_RSA(filename):
+    
     # generate RSA keys
     key = RSA.generate(3072)
 
@@ -15,10 +15,13 @@ def encrypt_file_RSA(filename):
     # read in the file
     with open(filename, 'rb') as file:
         data = file.read()
-
+    start_time = time.time()  
     # pad the data so that it is a multiple of the block size
     pad_size = block_size - len(data) % block_size
     data += pad_size.to_bytes(1, 'big') * pad_size
+    # start the encryption timer
+   
+  
 
     # initialize the RSA cipher in CBC mode
     cipher_rsa = PKCS1_OAEP.new(key)
@@ -28,14 +31,14 @@ def encrypt_file_RSA(filename):
     cipher_aes = AES.new(session_key, AES.MODE_CBC)
 
     # encrypt the session key with the RSA cipher
-    start_time = time.time()
     encrypted_session_key = cipher_rsa.encrypt(session_key)
-    encryption_time = time.time() - start_time
 
     # encrypt the data with the AES cipher
     ciphertext = cipher_aes.encrypt(data)
 
-    # get the initial and encrypted file size
+    # calculate the full encryption time
+    
+    # Get the initial and encrypted file size
     initial_size = os.path.getsize(filename)
     encrypted_size = len(encrypted_session_key) + len(ciphertext)
 
@@ -44,48 +47,45 @@ def encrypt_file_RSA(filename):
     with open(encrypted_filename, 'wb') as file:
         file.write(encrypted_session_key)
         file.write(ciphertext)
+    encryption_time = time.time() - start_time
 
-# ... (the beginning part of the script remains unchanged)
+    # start the decryption timer
+    start_time = time.time()
 
-    # write the metrics to a file
-    with open('RSA_encryption_metrics.txt', 'a') as file:
-        file.write(f"{filename}, {initial_size}, {encrypted_size}, {encryption_time}, ")
+    # decrypt the session key
+    cipher_rsa_dec = PKCS1_OAEP.new(key)
+    decrypted_session_key = cipher_rsa_dec.decrypt(encrypted_session_key)
 
     # Decrypt the data
-    cipher_rsa_dec = PKCS1_OAEP.new(key)
-    start_time = time.time()
-    decrypted_session_key = cipher_rsa_dec.decrypt(encrypted_session_key)
-    decryption_time = time.time() - start_time
-
     cipher_aes_dec = AES.new(decrypted_session_key, AES.MODE_CBC)
     decrypted_data = cipher_aes_dec.decrypt(ciphertext)
 
-    # remove padding from decrypted data
+    # Calculate the full decryption time
+  
+    # Remove padding from decrypted data
     pad_size = decrypted_data[-1]
     decrypted_data = decrypted_data[:-pad_size]
+    
 
-    # write the decrypted data to a new file
+    # Write the decrypted data to a new file
     decrypted_filename = filename + '.dec'
     with open(decrypted_filename, 'wb') as file:
         file.write(decrypted_data)
-
-    # write decryption time to the file
+    decryption_time = time.time() - start_time
+    # Write the metrics to a file
     with open('RSA_encryption_metrics.txt', 'a') as file:
-        file.write(f"{decryption_time}\n")
+        file.write(f"{filename}, {initial_size}, {encrypted_size}, {encryption_time}, {decryption_time}\n")
 
-# ... (the rest of the script remains unchanged)
-
-
-# define the file extensions to be encrypted
+# Define the file extensions to be encrypted
 extensions = ['.pdf', '.txt', '.mp3', '.docx']
 
-# prompt user for number of runs
+# Prompt user for number of runs
 num_runs = int(input("Enter the number of times to run the encryption algorithm: "))
 
 for i in range(num_runs):
     print(f"--- Run {i+1} ---")
 
-    # get all files in the current directory with the specified extensions
+    # Get all files in the current directory with the specified extensions
     for extension in extensions:
         for filename in os.listdir():
             if filename.endswith(extension):
