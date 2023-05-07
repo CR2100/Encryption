3fb20b1615d3ca7fc2aa8369ebf5ad49fa6d985d
CR2100 <104399936+CR2100@users.noreply.github.com>
2023-05-07 10:12:23 -0500

    Final Version Update

--- Blowfish.py
+++ Blowfish.py
@@ -3,91 +3,82 @@ import time
 from Crypto.Cipher import Blowfish
 from Crypto.Random import get_random_bytes
 
-# Define the encryption function for each file type
 def encrypt_file(filename):
-    # Set the encryption key
+    # generate 128-bit key using get_random_bytes()
     key = get_random_bytes(16)
 
-    # Read in the file
+    # read file to be encrypted
     with open(filename, 'rb') as file:
         data = file.read()
 
-    # Set the block size for the encryption
+    # pad the data to a multiple of the block size of Blowfish cipher
     block_size = Blowfish.block_size
-
-    # Pad the data so that it is a multiple of the block size
     pad_size = block_size - len(data) % block_size
     data += pad_size.to_bytes(1, 'big') * pad_size
 
-    # Initialize the Blowfish cipher in CBC mode
+    # encrypt the data using the key and CBC mode
     cipher = Blowfish.new(key, Blowfish.MODE_CBC)
-
-    # Encrypt the data
     start_time = time.time()
     ciphertext = cipher.encrypt(data)
     encryption_time = time.time() - start_time
 
-    # Get the initial and encrypted file size
+    # record the original size and encrypted size of the file
     initial_size = os.path.getsize(filename)
     encrypted_size = len(ciphertext)
 
-    # Write the encrypted data to a new file
+    # write encrypted data to a new file with '.enc' extension
     encrypted_filename = filename + '.enc'
     with open(encrypted_filename, 'wb') as file:
         file.write(cipher.iv + ciphertext)
 
-    # Decrypt the data
-    with open(encrypted_filename, 'rb') as file:
+    # return the key, encrypted filename, initial size, encrypted size, and encryption time
+    return key, encrypted_filename, initial_size, encrypted_size, encryption_time
+
+
+def decrypt_file(filename, key):
+    # read encrypted data from the input file
+    with open(filename, 'rb') as file:
         encrypted_data = file.read()
 
+    # separate the IV from the ciphertext
     iv = encrypted_data[:8]
     ciphertext = encrypted_data[8:]
 
-    cipher_dec = Blowfish.new(key, Blowfish.MODE_CBC, iv)
-    decrypted_data = cipher_dec.decrypt(ciphertext)
-
-    # Remove the padding from the decrypted data
-    decrypted_data = decrypted_data[:-decrypted_data[-1]]
-
-    # Write the decrypted data to a new file
-    decrypted_filename = filename + '.dec'
-    with open(decrypted_filename, 'wb') as file:
-        file.write(decrypted_data)
-
-# ... (the beginning part of the script remains unchanged)
-
-    # Decrypt the data
-    with open(encrypted_filename, 'rb') as file:
-        encrypted_data = file.read()
-
-    iv = encrypted_data[:8]
-    ciphertext = encrypted_data[8:]
-
+    # decrypt the data using the key, IV, and CBC mode
     cipher_dec = Blowfish.new(key, Blowfish.MODE_CBC, iv)
     start_time = time.time()
     decrypted_data = cipher_dec.decrypt(ciphertext)
     decryption_time = time.time() - start_time
 
-    # ... (the rest of the script remains unchanged)
+    # Unpad the decrypted data and write it to a new file with '.dec' extension
+    decrypted_data = decrypted_data[:-decrypted_data[-1]]
+    decrypted_filename = filename[:-4] + '.dec'
+    with open(decrypted_filename, 'wb') as file:
+        file.write(decrypted_data)
 
-    # Write the metrics to a file
-    with open('Blowfish_encryption_metrics.txt', 'a') as file:
-        file.write(f"{filename}, {initial_size}, {encrypted_size}, {encryption_time}, {decryption_time}\n")
-
-# ... (the rest of the script remains unchanged)
+    # Return the decryption time
+    return decryption_time
 
 
-# Define the file extensions to be encrypted
+# Define a list of file extensions to be encrypted
 extensions = ['.pdf', '.txt', '.mp3', '.docx']
 
-# Get the number of times to run the encryption
+# Get the number of times to run the encryption from the user
 num_runs = int(input("How many times would you like to run the encryption? "))
 
-# Run the encryption and decryption for each file type and repeat as many times as specified
+# Loop through the encryption and decryption process for each file
 for _ in range(num_runs):
     print(f"--- Run {_+1} ---")
     for extension in extensions:
         for filename in os.listdir():
             if filename.endswith(extension):
-                encrypt_file(filename)
+                # Encrypt the file and record the metrics
+                key, encrypted_filename, initial_size, encrypted_size, encryption_time = encrypt_file(filename)
+                decryption_time = decrypt_file(encrypted_filename, key)
+
+                # Write the metrics to a file
+                with open('Blowfish_encryption_metrics.txt', 'a') as file:
+                    file.write(f"{filename}, {initial_size}, {encrypted_size}, {encryption_time}, {decryption_time}\n")
+
+# Print a message indicating that the process is complete
 print("Process complete.")
